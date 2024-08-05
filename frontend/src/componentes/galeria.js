5ef4e3534ef1f7efc41e0165418a412e579d7868
Yesly Valentina Quintero <yesly.quintero@cens.com.co>
2024-08-05 17:54:52 -0500

    Nuevos cambios

--- frontend/src/componentes/galeria.js
+++ frontend/src/componentes/galeria.js
@@ -1,81 +1,77 @@
 import React from 'react';
-
 import imagen1 from './assets/imagen/gal-imagen1.jpg';
 import imagen2 from './assets/imagen/gal-imagen2.jpg';
 import imagen3 from './assets/imagen/gal-imagen3.jpg';
 import imagen4 from './assets/imagen/gal-imagen4.jpg';
+import './style/Galeria.css';
 
-
-function Galeria () {
+function Galeria() {
     return (
-        
-        <div class="row row-cols-1 row-cols-md-2 g-4">
-
-            <div class="col">
-                <div class="card">
-                <img src= {imagen1} class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Imagen 1</h5> <br />
-                    <p class="card-text">Partido entre profesores y alumnos.</p>
+        <div className="container">
+            <br></br>
+            <br></br>
+            <br></br>
+            <div className="row row-cols-1 row-cols-md-2 g-4">
+                <div className="col">
+                    <div className="card">
+                        <img src={imagen1} className="card-img-top" alt="Imagen 1" />
+                        <div className="card-body">
+                            <h5 className="card-title">Imagen 1</h5>
+                            <p className="card-text">Partido entre profesores y alumnos.</p>
+                        </div>
+                    </div>
                 </div>
+
+                <div className="col">
+                    <div className="card">
+                        <img src={imagen2} className="card-img-top" alt="Imagen 2" />
+                        <div className="card-body">
+                            <h5 className="card-title">Imagen 2</h5>
+                            <p className="card-text">Juega, sueña, diviértete, aprende, siéntete orgulloso de jugar este deporte.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col">
+                    <div className="card">
+                        <img src={imagen3} className="card-img-top" alt="Imagen 3" />
+                        <div className="card-body">
+                            <h5 className="card-title">Imagen 3</h5>
+                            <p className="card-text">Primer gol de los profesores.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col">
+                    <div className="card">
+                        <img src={imagen4} className="card-img-top" alt="Imagen 4" />
+                        <div className="card-body">
+                            <h5 className="card-title">Imagen 4</h5>
+                            <p className="card-text">Qué partidazo de los profesores con los alumnos.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col">
+                    <div className="card video">
+                        <iframe width="100%" height="auto" src="https://www.youtube.com/embed/xW1qwTbHlSs" title="Video Microfútbol" allowFullScreen></iframe>
+                        <div className="card-body">
+                            <h5 className="card-title">Microfútbol - Col. General Santander</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col">
+                    <div className="card video">
+                        <iframe width="100%" height="auto" src="https://www.youtube.com/embed/ywMk1g5IzoQ" title="Video ajedrez" allowFullScreen></iframe>
+                        <div className="card-body">
+                            <h5 className="card-title">Ajedrez - Col. General Santander</h5>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <div class="col">
-                <div class="card">
-                <img src= {imagen2} class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Imagen 2</h5> <br />
-                    <p class="card-text">Juega, sueña, diviertete, aprende, sientete orgulloso del jugar este deporte.</p>
-                </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="card">
-                <img src= {imagen3} class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Imagen 3</h5> <br />
-                    <p class="card-text">Primer gol de los profesores.</p>
-                </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="card">
-                <img src= {imagen4} class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Imagen 4</h5> <br />
-                    <p class="card-text">Que partidazo de los profesores con los alumnos.</p>
-                </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="video">
-                <iframe width="848" height="480" src="https://www.youtube.com/embed/xW1qwTbHlSs" title="Video Microfútbol" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                <div class="card-body">
-                    <h5 class="card-title">Microfútbol - Col. General Santander</h5> <br />
-
-                </div>
-                </div>
-            </div>
-
-            <div class="col">
-                <div class="video">
-                <iframe width="337" height="599" src="https://www.youtube.com/embed/ywMk1g5IzoQ" title="Video ajedrez" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                <div class="card-body">
-                    <h5 class="card-title">Ajedrez - Col. General Santander</h5> <br />
-                    
-                </div>
-                </div>
-            </div>
-
-             
-            
-           
         </div>
-    )
+    );
 }
 
-export default Galeria
+export default Galeria;
